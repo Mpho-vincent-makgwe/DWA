609576c04bad2422111c6a4849ba6edd1cb532c1
Mpho Vincent Makgwe <mphomakgwe4@gmail.com>
2023-06-21 16:21:36 +0200

    Done with DWA10

---
+++ src/DWA10/script.js
@@ -0,0 +1,30 @@
+
+import './component.js';
+const counterValueInput = document.querySelector(".counter__value");
+const addButton = document.querySelector('[data-key="add"]');
+const subtractButton = document.querySelector('[data-key="subtract"]');
+const resetButton = document.querySelector('[data-key="reset"]');
+
+let counterValue = 0;
+
+function updateCounterValue() {
+    counterValueInput.value = counterValue;
+}
+
+addButton.addEventListener("click", () => {
+    counterValue++;
+    updateCounterValue();
+});
+
+subtractButton.addEventListener("click", () => {
+    if (counterValue > 0) {
+        counterValue--;
+        updateCounterValue();
+    }
+});
+
+resetButton.addEventListener("click", () => {
+    counterValue = 0;
+    updateCounterValue();
+    alert("Counter has been reset.");
+});
