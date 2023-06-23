988b58a44b7a94c319714aa0f4c89e20e1d433c7
Mpho Vincent Makgwe <mphomakgwe4@gmail.com>
2023-06-23 11:48:42 +0200

    commented my switch statement in

--- src/DWA11/reducers.js
+++ src/DWA11/reducers.js
@@ -24,6 +24,17 @@ return actionHandler ? actionHandler() : state;
 // };
 
 
-
+// export const reducer = (state = { count: 0 }, action) => {
+//     switch (action.type) {
+//       case 'ADD':
+//         return { count: state.count + 1 };
+//       case 'SUBTRACT':
+//         return { count: state.count - 1 };
+//       case 'RESET':
+//         return { count: 0 };
+//       default:
+//         return state;
+//     }
+//   };
 
 
